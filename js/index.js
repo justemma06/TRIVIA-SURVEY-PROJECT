830cd3f6d1f890cd6e69d44e603169a7346c25b3
Emmanuel Emmanuel <imma2005nuel@gmail.com>
2023-06-07 21:58:52 +0100

    add getCurrentTime and date function

--- js/index.js
+++ js/index.js
@@ -222,4 +222,30 @@ function getElements(response) {
     })
 }   
 
+function getCurrentTime() {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = 0;
+    if (now.getMinutes() + 1 < 10) {
+        minutes = '0' + now.getMinutes().toString();
+    } else {
+        minutes = now.getMinutes();
+    }
+    let period;
+    if (hours >= 12) {
+        period = 'PM';
+    } else {
+        period = 'AM';
+    }
+    let date = `${now.getMonth() + 1}/${now.getDate()}/${now.getFullYear()}`;
+    let time;
+    if (hours > 12) {
+        time = `${hours - 12}:${minutes} ${period}`;
+    } else if (hours == 0) {
+        time = `${hours + 12}:${minutes} ${period}`;
+    } else {
+        time = `${hours}:${minutes} ${period}`;
+    }
+    return `${date} ${time}`;
+};
 
