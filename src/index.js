dd9473a9e21217e64b83757a5fee4ed1c25b234e
Emmanuel Emmanuel <imma2005nuel@gmail.com>
2023-06-08 12:26:02 +0100

    add other extensions installed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+`http://localhost:8080/images/Purple Diamond.svg`;
+`http://localhost:8080/images/Orange Circle.svg`;
 
 function getElements(response) {
   // $('#question').html(response.results[0].question);
